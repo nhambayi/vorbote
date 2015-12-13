9ee95fab7054be4cd24aa343380254c97a53f438
Noah Hambayi <noah@hambayi.com>
2015-12-12 18:45:59 -0800

    changs

--- Vorbote.TestHost/Program.cs
+++ Vorbote.TestHost/Program.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Vorbote.Accounts;
-
-namespace Vorbote.TestHost
+﻿namespace Vorbote.TestHost
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Vorbote.Accounts;
+
     class Program
     {
         static  void Main(string[] args)
         {
-            DocumentDbMailBoxRepositry db = new DocumentDbMailBoxRepositry();
-            db.Initialize();
-
-            var mail = db.GetMailBox("noahhambayi");
             Server server = new Server(true);
 
             server.Init();
