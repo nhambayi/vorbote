f805666da84b40dbbbfd5a5634b11393ec3c9ad2
Noah Hambayi <noah@hambayi.com>
2016-01-10 17:14:30 -0800

    removed unnecessary projects

--- Vorbote.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Vorbote.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Vorbote.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vorbote.Api.WebApiApplication" Language="C#" %>
