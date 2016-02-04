3ef624d97a772accf14f4d467d657c0f70f47a60
Noah Hambayi <noah@hambayi.com>
2016-02-03 16:32:50 -0800

    added missing files

---
+++ Vorbote.Mailboxes/IMailboxRepositry.cs
@@ -0,0 +1,16 @@
+﻿namespace Vorbote.Mailboxes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IMailboxRepositry
+    {
+        void AddNewMailbox(IMailbox mailbox);
+
+        IMailbox GetMailbox(string username);
+
+    }
+}
